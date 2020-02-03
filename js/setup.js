c277876e85d369ca8e447793e16d5b1a389907da
Uporold <crubick1337@gmail.com>
2020-02-03 15:35:10 +0300

    Открытие/закрытие окна параметров, валидация, изменение внешности мага по клику.

--- js/setup.js
+++ js/setup.js
@@ -4,7 +4,11 @@ var WIZARD_NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'К
 var WIZARD_SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var WIZARD_COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var WIZARD_EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
+var WIZARD_FIREBALL_COLORS = ['#ee4830', '#30a8ee', '#5ce6c0', '#e848d5', '#e6e848'];
 var OBJECTS_AMOUNT = 4;
+
+var ESC_KEY = 'Escape';
+var ENTER_KEY = 'Enter';
 
 var wizards = [];
 
@@ -32,8 +36,6 @@ var createWizardsArray = function (amount) {
 };
 
 var userDialog = document.querySelector('.setup');
-userDialog.classList.remove('hidden');
-
 var similarListElement = userDialog.querySelector('.setup-similar-list');
 
 var similarWizardTemplate = document.querySelector('#similar-wizard-template')
@@ -63,3 +65,72 @@ var renderSimilarWizards = function () {
 renderSimilarWizards();
 
 userDialog.querySelector('.setup-similar').classList.remove('hidden');
+
+var setup = document.querySelector('.setup');
+var setupOpen = document.querySelector('.setup-open');
+var setupClose = setup.querySelector('.setup-close');
+
+var onPopupEscPress = function (evt) {
+  if (evt.key === ESC_KEY) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
+  setup.classList.remove('hidden');
+  document.addEventListener('keydown', onPopupEscPress);
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    openPopup();
+  }
+});
+
+setupClose.addEventListener('click', function () {
+  closePopup();
+});
+
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.key === ENTER_KEY) {
+    closePopup();
+  }
+});
+
+var setupWizard = document.querySelector('.setup-wizard');
+var wizardCoat = setupWizard.querySelector('.wizard-coat');
+var wizardEyes = setupWizard.querySelector('.wizard-eyes');
+var setupFireball = document.querySelector('.setup-fireball-wrap');
+
+var changeCoatColor = function () {
+  wizardCoat.style.fill = generateRandomWizard().coatColor;
+};
+
+var changeEyesColor = function () {
+  wizardEyes.style.fill = generateRandomWizard().eyesColor;
+};
+
+var changeFireballColor = function () {
+  setupFireball.style.backgroundColor = WIZARD_FIREBALL_COLORS[getRandomItem(WIZARD_FIREBALL_COLORS)];
+};
+
+wizardCoat.addEventListener('click', function () {
+  changeCoatColor();
+});
+
+wizardEyes.addEventListener('click', function () {
+  changeEyesColor();
+});
+
+setupFireball.addEventListener('click', function () {
+  changeFireballColor();
+});
